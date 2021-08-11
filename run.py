85c61190def5ade7f2a9e54e0238354256122e9a
Joseph Woodland <joseph@workit-app.com>
2021-08-11 14:44:32 +0100

    Sales data is checked and returned by the function

--- run.py
+++ run.py
@@ -16,13 +16,18 @@ def get_sales_data():
     """
     Get sales figures input from the user
     """
-    print('Please enter sales data from the last market.')
-    print('Data should be six numbers, seperated by commas')
-    print('Example : 10,20,30,30,40,10,\n')
+    while True:
+        print('Please enter sales data from the last market.')
+        print('Data should be six numbers, seperated by commas')
+        print('Example : 10,20,30,30,40,10,\n')
 
-    data_str = input('Enter your data here: ')
-    sales_data = data_str.split(',')
-    validate_data(sales_data)
+        data_str = input('Enter your data here: ')
+        sales_data = data_str.split(',')
+        
+
+        if validate_data(sales_data):
+            print('Data is valid')
+            break
 
 def validate_data(values):
     """
@@ -31,7 +36,7 @@ def validate_data(values):
     or if there aren't exactly 6 values
     """
     try:
-        
+
         [int(value) for value in values]
 
         if len(values) != 6:
@@ -39,6 +44,10 @@ def validate_data(values):
                 f'Exactly 6 values required, you provided {len(values)} '
             )
     except ValueError as e:
-        print(f'Invalid data: {e}, pleae try again \n')
+        print(f'Invalid data: {e}, please try again \n')
+        return False
+
+    return True
+
 
 get_sales_data()
